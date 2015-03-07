a64db9501d7d6edd936800a9ec008fa2d7b45a59
Chris Lewis <chris.lewis2@gmail.com>
2015-03-07 11:11:17 -0500

    contiuing to add api calls and tests

--- iterable.php
+++ iterable.php
@@ -5,204 +5,223 @@
  */
 
 class Iterable {
-  var $api_key = '';
-  var $api_url = 'https://api.iterable.com:443/api/';
-  var $debug = false;
+    private $api_key = '';
+    private $api_url = 'https://api.iterable.com:443/api/';
+    private $debug = false;
 
-  public function __construct( $api_key = false ) {
-    if( $api_key ) {
-      $this->api_key = $api_key;
-    }
-  }
+    private function query_string( $query ) {
+        $query_array = array();
 
-  public function query_string( $query ) {
-    $query_array = array();
+            foreach( $query as $key => $key_value ) {
+                $query_array[] = urlencode( $key ) . '=' . urlencode( $key_value );
+            }
 
-    foreach( $query as $key => $key_value ) {
-      $query_array[] = urlencode( $key ) . '=' . urlencode( $key_value );
+        return implode( '&', $query_array );
     }
 
-    return implode( '&', $query_array );
-  }
+    private function send_request( $resource, $params = array(), $request = 'GET' ) {
+        $curl_handle = curl_init();
 
-  public function send_request( $resource, $params = array(), $request = 'GET' ) {
-    $curl_handle = curl_init();
+        $url = $this->api_url . $resource . '?api_key=' . $this->api_key;
 
-    $url = $this->api_url . $resource . '?api_key=' . $this->api_key;
+        if( $request == 'GET' ) {
+            $url .= $this->query_string( $params );
+        } else if( $request == 'POST' ) {
+            curl_setopt( $curl_handle, CURLOPT_POSTFIELDS, $params );
+            curl_setopt( $curl_handle, CURLOPT_POST, 1 );
+        } else {
+            throw new Exception( 'Invalid request parameter specified.' );
+        }
 
-    if( $request == 'GET' ) {
-      $url .= $this->query_string( $params );
-    } else if( $request == 'POST' ) {
-      curl_setopt( $curl_handle, CURLOPT_POSTFIELDS, $params );
-      curl_setopt( $curl_handle, CURLOPT_POST, 1 );
-    } else {
-      throw new Exception( 'Invalid request parameter specified.' );
+        if( $this->debug ) {
+            curl_setopt( $curl_handle, CURLOPT_VERBOSE, true );
+        }
+
+        curl_setopt( $curl_handle, CURLOPT_URL, $url );
+        curl_setopt( $curl_handle, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt( $curl_handle, CURLOPT_SSL_VERIFYPEER, false );
+        curl_setopt( $curl_handle, CURLOPT_SSL_VERIFYHOST, 2 );
+        curl_setopt( $curl_handle, CURLOPT_TIMEOUT, 5 );
+
+        $buffer = curl_exec( $curl_handle );
+
+        if( $this->debug ) {
+            var_dump( $buffer );
+        }
+
+        $result = array(
+            'response_code' => curl_getinfo( $curl_handle, CURLINFO_HTTP_CODE ),
+        );
+
+        if( $result[ 'response_code' ] === 200 ) {
+            $result[ 'success' ] = true;
+            $result[ 'content' ] = json_decode( $buffer );
+        } else {
+            $result[ 'success' ] = false;
+            $result[ 'error_message' ] = $buffer;
+        }
+
+        return $result;
     }
 
-    if( $this->debug ) {
-      curl_setopt( $curl_handle, CURLOPT_VERBOSE, true );
+    public function __construct( $api_key, $debug = false ) {
+        $this->api_key = $api_key;
+        $this->debug = $debug;
     }
 
-    curl_setopt( $curl_handle, CURLOPT_URL, $url );
-    curl_setopt( $curl_handle, CURLOPT_RETURNTRANSFER, 1 );
-    curl_setopt( $curl_handle, CURLOPT_SSL_VERIFYPEER, false );
-    curl_setopt( $curl_handle, CURLOPT_SSL_VERIFYHOST, 2 );
-    curl_setopt( $curl_handle, CURLOPT_TIMEOUT, 5 );
+    /* Lists */
 
-    $buffer = curl_exec( $curl_handle );
+    public function lists() {
+        $result = $this->send_request( 'lists' );
+        if( $result[ 'success' ] ) {
+            $result[ 'content' ] = array_map( get_object_vars, $result[ 'content' ]->lists );
+        }
 
-    if( $this->debug ) {
-      var_dump( $buffer );
+        return $result;
     }
 
-    $result = array(
-      'response_code' => curl_getinfo( $curl_handle, CURLINFO_HTTP_CODE ),
-    );
-
-    if( $result[ 'response_code' ] === 200 ) {
-      $result[ 'success' ] = true;
-      $result[ 'content' ] = json_decode( $buffer );
-    } else {
-      $result[ 'success' ] = false;
-      $result[ 'error_message' ] = $buffer;
+    public function list_subscribe( $list_id, $subscribers, $resubscribe = false ) {
+        $body = array(
+            'listId' => (int) $list_id,
+            'subscribers' => $subscribers,
+            'resubscribe' => $resubscribe
+        );
+        return $this->send_request( 'lists/subscribe', json_encode( $body ), 'POST' );
     }
 
-    return $result;
-  }
+    public function list_unsubscribe( $list_id, $subscribers, $campaign_id = false, $channel_unsubscribe = false ) {
+        $request = array(
+            'listId' => (int) $list_id,
+            'subscribers' => $subscribers,
+        );
 
-  /* Lists */
+        // optionals
+        if( $campaign_id ) {
+            $request[ 'campaignId' ] = $campaign_id;
+        }
+        if( $channel_unsubscribe ) {
+            $request[ 'channelUnsubscribe' ] = $channel_unsubscribe;
+        }
 
-  public function lists() {
-    $result = $this->send_request( 'lists' );
-                if( $result[ 'success' ] ) {
-                        $result[ 'content' ] = array_map( get_object_vars, $result[ 'content' ]->lists );
-                }
-
-                return $result;
-  }
-
-  public function list_subscribe( $list_id, $subscribers, $resubscribe = false ) {
-    $body = array(
-                        'listId' => (int) $list_id,
-                        'subscribers' => $subscribers,
-                        'resubscribe' => $resubscribe
-                );
-    return $this->send_request( 'lists/subscribe', json_encode( $body ), 'POST' );
-  }
-
-  public function list_unsubscribe( $list_id, $subscribers, $campaign_id = false, $channel_unsubscribe = false ) {
-    $request = array(
-      'listId' => (int) $list_id,
-      'subscribers' => $subscribers
-    );
-
-    // optionals
-    if( $campaign_id ) {
-      $request[ 'campaignId' ] = $campaign_id;
-    }
-    if( $channel_unsubscribe ) {
-      $request[ 'channelUnsubscribe' ] = $channel_unsubscribe;
+        return $this->send_request( 'lists/unsubscribe', json_encode( $request ), 'POST' );
     }
 
-    return $this->send_request( 'lists/unsubscribe', json_encode( $request ), 'POST' );
-  }
+    /* Events */
 
-  /* Events */
-
-  public function event_track() {
-    throw new Exception( 'Not yet implemented' );
-  }
-
-  public function event_track_conversation() {
-    throw new Exception( 'Not yet implemented' );
-  }
-
-  /* Users */
-
-  public function user( $email ) {
-    $result = $this->send_request( 'users/get', json_encode( array(
-      'email' => $email
-    ) ), 'POST' );
-    if( $result[ 'success' ] ) {
-      $result[ 'content' ] = get_object_vars( $result[ 'content' ]->user->dataFields );
-    }
-    return $result;
-  }
-
-  public function user_delete( $email ) {
-    $result = $this->send_request( 'users/delete', json_encode( array(
-      'email' => $email
-    ) ), 'POST' );
-    return $result;
-  }
-
-  public function user_update_email( $current_email, $new_email ) {
-    $result = $this->send_request( 'users/updateEmail', json_encode( array(
-      'currentEmail' => $current_email,
-      'newEmail' => $new_email
-    ) ), 'POST' );
-    return $result;
-  }
-
-  public function user_bulk_update( $users ) {
-    throw new Exception( 'Not yet implemented' );
-  }
-
-  public function user_update_subscriptions( $user ) {
-    throw new Exception( 'Not yet implemented' );
-  }
-
-  public function user_fields() {
-    $result = $this->send_request( 'users/getFields' );
-
-    if( $result[ 'success' ] ) {
-      $result[ 'content' ] = array_keys( get_object_vars( $result[ 'content' ]->fields ) );
+    public function event_track() {
+        throw new Exception( 'Not yet implemented' );
     }
 
-    return $result;
-  }
+    public function event_track_conversation() {
+        throw new Exception( 'Not yet implemented' );
+    }
 
-  public function user_update( $user ) {
-    throw new Exception( 'Not yet implemented' );
-  }
+    /* Users */
 
-  /* Campaigns */
+    public function user( $email ) {
+        $result = $this->send_request( 'users/get', json_encode( array(
+            'email' => $email
+        ) ), 'POST' );
 
-  public function campaigns() {
-    throw new Exception( 'Not yet implemented' );
-  }
+        if( $result[ 'success' ] ) {
+            $result[ 'content' ] = get_object_vars( $result[ 'content' ]->user->dataFields );
+        }
 
-  /* Commerce */
+        return $result;
+    }
 
-  public function commerce_track_purchase( $user, $items, $campaign_id = false, $template_id = false, $data_fields = false ) {
-    throw new Exception( 'Not yet implemented' );
-  }
+    public function user_delete( $email ) {
+        $result = $this->send_request( 'users/delete', json_encode( array(
+            'email' => $email
+        ) ), 'POST' );
+        return $result;
+    }
 
-  public function commerce_update_cart( $user, $items ) {
-    throw new Exception( 'Not yet implemented' );
-  }
+    public function user_update_email( $current_email, $new_email ) {
+        $result = $this->send_request( 'users/updateEmail', json_encode( array(
+            'currentEmail' => $current_email,
+            'newEmail' => $new_email
+        ) ), 'POST' );
+        return $result;
+    }
 
-  /* Email */
+    public function user_bulk_update( $users ) {
+        $result = $this->send_request( 'users/bulkUpdate', json_encode( array(
+            'users' => $users
+        ) ), 'POST' );
+        return $result;
+    }
 
-  public function email( $campaign_id, $recipient, $send_at = false, $inline_css = false, $attachments = false ) {
-    throw new Exception( 'Not yet implemented' );
-  }
+    public function user_update_subscriptions( $email, $email_list_ids = false, $unsubscribed_channel_ids = false, $unsubscribed_message_type_ids = false, $campaign_id = false, $template_id = false ) {
+        $request = array(
+            'email' => $email
+        );
 
-  /* Export */
+        // optional arguments
+        if( $email_list_ids ) $request[ 'emailListIds' ] = $email_list_ids;
+        if( $unsubscribed_channel_ids ) $request[ 'unsubscribedChannelIds' ] = $unsubscribed_channel_ids;
+        if( $unsubscribed_message_type_ids ) $request[ 'unsubscribedMessageTypeIds' ] = $unsubscribed_message_type_ids;
+        if( $campaign_id ) $request[ 'campaignId' ] = $campaign_id;
+        if( $template_it ) $request[ 'templateId' ] = $template_id;
 
-  public function export_json() {
-    throw new Exception( 'Not yet implemented' );
-  }
+        $result = $this->send_request( 'users/updateSubscriptions', json_encode( $request ), 'POST' );
+        return $result;
+    }
 
-  public function export_csv() {
-    throw new Exception( 'Not yet implemented' );
-  }
+    public function user_fields() {
+        $result = $this->send_request( 'users/getFields' );
 
-  /* Workflows */
+        if( $result[ 'success' ] ) {
+            $result[ 'content' ] = array_keys( get_object_vars( $result[ 'content' ]->fields ) );
+        }
 
-  public function trigger_workflow() {
-    throw new Exception( 'Not yet implemented' );
-  }
+        return $result;
+    }
+
+    public function user_update( $email, $data_fields, $user_id ) {
+        $result = $this->send_request( 'users/update', json_encode( array(
+            'email' => $email,
+            'dataFields' => $data_fields,
+            'user_id' => $user_id
+        ) ), 'POST' );
+        return $result;
+    }
+
+    /* Campaigns */
+
+    public function campaigns() {
+        return $this->send_request( 'campaigns' );
+    }
+
+    /* Commerce */
+
+    public function commerce_track_purchase( $user, $items, $campaign_id = false, $template_id = false, $data_fields = false ) {
+        throw new Exception( 'Not yet implemented' );
+    }
+
+    public function commerce_update_cart( $user, $items ) {
+        throw new Exception( 'Not yet implemented' );
+    }
+
+    /* Email */
+
+    public function email( $campaign_id, $recipient, $send_at = false, $inline_css = false, $attachments = false ) {
+        throw new Exception( 'Not yet implemented' );
+    }
+
+    /* Export */
+
+    public function export_json() {
+        throw new Exception( 'Not yet implemented' );
+    }
+
+    public function export_csv() {
+        throw new Exception( 'Not yet implemented' );
+    }
+
+    /* Workflows */
+
+    public function trigger_workflow() {
+        throw new Exception( 'Not yet implemented' );
+    }
 }
-
-?>
--- iterableTest.php
+++ iterableTest.php
@@ -3,57 +3,75 @@
 require_once( dirname( __FILE__ ) . '/iterable.php' );
 
 class iterableTest extends \PHPUnit_Framework_TestCase {
-  private $iterable;
-  private $subscribed_user;
-  public function __construct() {
-    $this->iterable = new Iterable( getenv( 'ITERABLE_KEY' ) );
-  }
+    private $iterable;
 
-  /* Lists */
-
-  public function testLists() {
-    $result = $this->iterable->lists();
-    $this->assertTrue( $result[ 'success' ] );
-  }
-  public function testListSubscribe() {
-    $lists = $this->iterable->lists();
-    $this->assertTrue( $lists[ 'success' ] );
-
-    if( count( $lists[ 'content' ] ) > 0 ) {
-      $result = $this->iterable->list_subscribe( $lists[ 'content' ][ 0 ][ 'id' ],
-        array( array( 'email' => 'test@example.com' ) ) );
-      $this->assertTrue( $result[ 'success' ] );
+    public function __construct() {
+        $this->iterable = new Iterable( getenv( 'ITERABLE_KEY' ) );
     }
-  }
 
-  public function testListUnsubscribe() {
-    $user = $this->iterable->user( 'test@example.com' );
-    $this->assertTrue( $user[ 'success' ] );
+    /* Lists */
 
-    foreach( $user[ 'content' ][ 'emailListIds' ] as $list_id ) {
-      $response = $this->iterable->list_unsubscribe( $list_id,
-        array( array( 'email' => $user[ 'content' ][ 'email' ] ) ) );
-      $this->assertTrue( $response[ 'success' ] );
+    public function testLists() {
+        $result = $this->iterable->lists();
+        $this->assertTrue( $result[ 'success' ] );
     }
-  }
 
-  /* User */
+    public function testListSubscribe() {
+        $lists = $this->iterable->lists();
+        $this->assertTrue( $lists[ 'success' ] );
 
-  public function testUserGet() {
-    $user = $this->iterable->user( 'test@example.com' );
-    $this->assertTrue( $user[ 'success' ] );
-  }
+        if( count( $lists[ 'content' ] ) > 0 ) {
+            $result = $this->iterable->list_subscribe( $lists[ 'content' ][ 0 ][ 'id' ],
+                array( array( 'email' => 'test@example.com' ) ) );
+            $this->assertTrue( $result[ 'success' ] );
+        }
+    }
 
-  public function testUpdateEmail() {
-    // make sure user doesn't already exist
-    $this->iterable->user_delete( 'test2@example.com' );
+    public function testListUnsubscribe() {
+        $user = $this->iterable->user( 'test@example.com' );
+        $this->assertTrue( $user[ 'success' ] );
 
-    $response = $this->iterable->user_update_email( 'test@example.com', 'test2@example.com' );
-    $this->assertTrue( $response[ 'success' ] );
-  }
+        foreach( $user[ 'content' ][ 'emailListIds' ] as $list_id ) {
+            $response = $this->iterable->list_unsubscribe( $list_id,
+                array( array( 'email' => $user[ 'content' ][ 'email' ] ) ) );
+            $this->assertTrue( $response[ 'success' ] );
+        }
+    }
 
-  public function testUserDelete() {
-    $response = $this->iterable->user_delete( 'test2@example.com' );
-    $this->assertTrue( $response[ 'success' ] );
-  }
+    /* User */
+
+    public function testUserGet() {
+        $user = $this->iterable->user( 'test@example.com' );
+        $this->assertTrue( $user[ 'success' ] );
+    }
+
+    public function testUpdateEmail() {
+        // make sure user doesn't already exist
+        $this->iterable->user_delete( 'test2@example.com' );
+
+        $response = $this->iterable->user_update_email( 'test@example.com', 'test2@example.com' );
+        $this->assertTrue( $response[ 'success' ] );
+    }
+
+    public function testUserDelete() {
+        $response = $this->iterable->user_delete( 'test2@example.com' );
+        $this->assertTrue( $response[ 'success' ] );
+    }
+
+    public function testUserBulkUpdate() {
+        $response = $this->iterable->user_bulk_update( array(
+            array( 'email' => 'test1@example.com' ),
+            array( 'email' => 'test2@example.com' )
+        ) );
+
+        $this->iterable->user_delete( 'test1@example.com' );
+        $this->iterable->user_delete( 'test2@example.com' );
+
+        $this->assertTrue( $response[ 'success' ] );
+    }
+
+    public function testUserUpdateSubscriptions() {
+        $response = $this->iterable->user_update_subscriptions( 'test@example.com' );
+        $this->assertTrue( $response[ 'success' ] );
+    }
 }
